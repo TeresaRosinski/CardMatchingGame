4ff4b7a5b31c51e646f7a8d92698b484008ff5b3
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2021-10-06 20:29:36 -0500

    add front and back images to each card

--- scripts.js
+++ scripts.js
@@ -11,83 +11,83 @@ playerAttemptsCount.textContent = playerAttempts;
 //data
 const getCardData = () => [
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-1.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-1.png',
 		id: 1,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-2.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-2.png',
 		id: 2,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-3.png')`,
+		back:'./IMG/card-back.jpg',
+		front: './IMG/icon-3.png',
 		id: 3,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-4.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-4.png',
 		id: 4,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-5.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-5.png',
 		id: 5,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-6.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-6.png',
 		id: 6,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-7.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-7.png',
 		id: 7,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-8.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-8.png',
 		id: 8,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-1.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-1.png',
 		id: 1,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-2.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-2.png',
 		id: 2,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-3.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-3.png',
 		id: 3,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-4.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-4.png',
 		id: 4,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-5.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-5.png',
 		id: 5,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-6.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-6.png',
 		id: 6,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-7.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-7.png',
 		id: 7,
 	},
 	{
-		background: `url('./IMG/card-back.jpg')`,
-		front: `url('./IMG/icon-8.png')`,
+		back: './IMG/card-back.jpg',
+		front: './IMG/icon-8.png',
 		id: 8,
 	},
 ];
@@ -104,12 +104,25 @@ randomizeCards();
 
 function makeCards() {
 	const randomCardData = randomizeCards();
-	console.log(randomCardData);
-	for (let d of randomCardData) {
+	for (let data of randomCardData) {
+
+		//create card holder + cards
 		const cardHolder = document.createElement("DIV");
 		const card = document.createElement("DIV");
-		cardHolder.className = "card";
-		card.className = "card-back";
+		const front = document.createElement("img");
+		const back = document.createElement("img");
+		cardHolder.className = "card-holder";
+		card.classList = "card";
+		front.classList = "front";
+		back.classList = "back";
+		
+		//attach data
+		back.src = data.back;
+		front.src= data.front;
+
+		card.appendChild(front);
+		card.appendChild(back);
+		console.log(card);
 		cardHolder.appendChild(card);
 		section.appendChild(cardHolder);
 	}
